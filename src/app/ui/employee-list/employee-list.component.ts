acdcaa48983dd02a4d49587a0e31cf5c6ed5c675
MaDud <martaduda@interia.eu>
2022-11-19 23:11:30 +0100

    feat: employee list

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -5,13 +5,17 @@ import {
 } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { EmployeeModel } from '../../model/employee.model';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'employee-list',
   templateUrl: './employee-list.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent {
-  data$: Observable<EmployeeModel[] | null> = of(null);
+  constructor(private _client: HttpClient) {}
+
+  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
 }
