56f3a8ee36d23a7f62ade84dc63c5142a4068193
MaDud <martaduda@interia.eu>
2022-11-22 22:39:10 +0100

    add post form data

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
+import {map, Observable} from 'rxjs';
+import { PersonModel } from '../model/person.model';
+import { EmployeeFormModel } from '../model/employee-form.model';
 
 @Injectable()
 export class EmployeesService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAll():Observable<PersonModel[]> {
+  getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+  }
+
+  createNewEmployee(employee: EmployeeFormModel): Observable<void> {
+  return this._httpClient.post<EmployeeFormModel>('https://dummy.restapiexample.com/api/v1/create', {...employee}).pipe(map(_ => void 0))
   }
 }
--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {EmployeeFormModel} from "../../model/employee-form.model";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmployeesService } from '../../services/employees.service';
+import { EmployeeFormModel } from '../../model/employee-form.model';
 
 @Component({
   selector: 'employee-form',
@@ -15,7 +16,10 @@ export class EmployeeFormComponent {
     salary: new FormControl(null, [Validators.required, Validators.min(0)])
   });
 
-  onButtonSubmit(data: EmployeeFormModel) {
-    alert(`User was successfully added to the database. Email: ${data.email}, age: ${data.age}, salary: ${data.salary}`)
+  constructor(private _employeesService: EmployeesService) {
+  }
+
+  onFormSubmit(data: EmployeeFormModel) {
+    this._employeesService.createNewEmployee(data).subscribe({next:() => alert(`User was successfully added to the database. Email: ${data.email}, age: ${data.age}, salary: ${data.salary}`)})
   }
 }
